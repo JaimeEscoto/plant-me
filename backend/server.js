c38f92ef50631c7354b1bda16f0c126d7ac8bf43
Jaime Escoto <jaime.escoto@hotmail.com>
2025-10-12 11:57:38 -0600

    Increase body parser limits for photo uploads

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,17 @@ const errorHandler = require('./src/middleware/errorHandler');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(
+  bodyParser.json({
+    limit: '5mb',
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    limit: '5mb',
+    extended: true,
+  })
+);
 app.use(morgan('dev'));
 
 app.use('/api/auth', authRoutes);
